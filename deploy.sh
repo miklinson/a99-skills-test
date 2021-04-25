a0e3baa90052df616cde0911a010757330fcc2d1
Mik Linson <miklinson@gmail.com>
2021-04-25 15:44:58 +0800

    removed the profile for AWS CLI

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,15 @@ ACTION=$1
 if [ $ACTION == delete ]
 then
     echo "Running AWS Cloud Formation stack deletion..."
-    aws cloudformation delete-stack --stack-name $STACK_NAME --profile mik-ebw
+    aws cloudformation delete-stack --stack-name $STACK_NAME
 elif [ $ACTION == create ]
 then
     echo "Running AWS Cloud Formation stack creation..."
     STACK_ID=$(aws cloudformation create-stack --stack-name $STACK_NAME \
     --template-body $TEMPLATE \
-    --profile mik-ebw \
     | jq -r .StackId \
     )
     echo "Waiting for AWS Cloud Formation stack completion..."
-    aws cloudformation wait stack-create-complete --stack-name ${STACK_ID} --profile mik-ebw
-    aws cloudformation describe-stacks --stack-name ${STACK_NAME} --profile mik-ebw | jq .Stacks[0].Outputs
+    aws cloudformation wait stack-create-complete --stack-name ${STACK_ID}
+    aws cloudformation describe-stacks --stack-name ${STACK_NAME} | jq .Stacks[0].Outputs
 fi
