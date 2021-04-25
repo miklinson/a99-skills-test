769f806ad72471a3d902cab12da09e7b1f3535cf
Mik Linson <miklinson@gmail.com>
2021-04-25 15:30:34 +0800

    added network interfaces and user data in EC2::INSTANCE

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-echo "Running AWS Cloud Formation stack creation..."
-
 STACK_NAME="skills-test"
 TEMPLATE="file://vpc-ec2-sg.yaml"
+ACTION=$1
 
-aws cloudformation create-stack --stack-name $STACK_NAME \
---template-body $TEMPLATE \
---profile mik-ebw
+if [ $ACTION == delete ]
+then
+    echo "Running AWS Cloud Formation stack deletion..."
+    aws cloudformation delete-stack --stack-name $STACK_NAME --profile mik-ebw
+elif [ $ACTION == create ]
+then
+    echo "Running AWS Cloud Formation stack creation..."
+    STACK_ID=$(aws cloudformation create-stack --stack-name $STACK_NAME \
+    --template-body $TEMPLATE \
+    --profile mik-ebw \
+    | jq -r .StackId \
+    )
+    echo "Waiting for AWS Cloud Formation stack completion..."
+    aws cloudformation wait stack-create-complete --stack-name ${STACK_ID} --profile mik-ebw
+    aws cloudformation describe-stacks --stack-name ${STACK_NAME} --profile mik-ebw | jq .Stacks[0].Outputs
+fi
